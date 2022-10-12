76db6138309789dc677e96ae36d22dedcd077fe7
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-10-12 14:07:12 +0500

    Homework 4 done

--- Домашнее_задание/Семинар_4/task_5.py
+++ Домашнее_задание/Семинар_4/task_5.py
@@ -11,6 +11,60 @@
 path_1 = 'Task_4.txt'
 path_2 = 'Task_4_1.txt'
 
+def Separate(equation, multipliers, degrees):
+    mult_string = ''
+    mult_stop = 0
+    degr_string = ''
+    degr_stop = 0
+    
+    while equation != '':
+        mult = equation.find('X')
+        if mult == -1:
+            if equation[0] == '+':
+                for i in range(1, len(equation)):
+                    mult_string += equation[i]
+                multipliers.append(int(mult_string))
+                mult_string = ''
+            else:
+                for i in range(len(equation)):
+                    mult_string += equation[i]
+                multipliers.append(int(mult_string))
+                mult_string = ''
+            mult_stop = len(equation)
+        elif mult == 0 or (mult == 1 and equation[0] == '+'):
+            multipliers.append(1)
+            mult_stop = mult + 1
+        elif mult == 1 and equation[0] == '-':
+            multipliers.append(-1)
+            mult_stop = mult + 1
+        else:
+            for i in range(mult - 1):
+                mult_string += equation[i]
+            multipliers.append(int(mult_string))
+            mult_string = ''
+            mult_stop = mult + 1
+    
+        equation = equation[mult_stop:]
+    
+        degr_start = equation.find('**')
+    
+        if degr_start == -1 and len(equation) > 0:
+            degrees.append(1)
+            degrees.append(0)
+        elif degr_start != -1:
+            if equation.find('+', 1) != -1 and equation.find('-', 1) != -1:
+                degr_stop = min(equation.find('+', 1), equation.find('-', 1))
+            elif equation.find('+', 1) == -1:
+                degr_stop = equation.find('-', 1)
+            else:
+                degr_stop = equation.find('+', 1)
+    
+            for i in range(degr_start + 2, degr_stop):
+                degr_string += equation[i]
+            degrees.append(int(degr_string))
+            degr_string = ''
+            equation = equation[degr_stop:]
+
 with open(path_1, "r", encoding='UTF-8') as data:
     equation_1 = data.readline()
 
@@ -21,61 +75,106 @@ multipliers_1 = []
 multipliers_2 = []
 degrees_1 = []
 degrees_2 = []
-mult_string = ''
-mult_stop = 0
-degr_string = ''
-degr_stop = 0
 
 del_start = equation_1.find('>')
 equation_1 = equation_1[(del_start + 2):]
 
-while equation_1 != '':
-    mult = equation_1.find('X')
-    if mult == -1:
-        if equation_1[0] == '+':
-            for i in range(1, len(equation_1)):
-                mult_string += equation_1[i]
-            multipliers_1.append(int(mult_string))
-            mult_string = ''
-        else:
-            for i in range(len(equation_1)):
-                mult_string += equation_1[i]
-            multipliers_1.append(int(mult_string))
-            mult_string = ''
-        mult_stop = len(equation_1)
-    elif mult == 0 or (mult == 1 and equation_1[0] == '+'):
-        multipliers_1.append(1)
-        mult_stop = mult + 1
-    elif mult == 1 and equation_1[0] == '-':
-        multipliers_1.append(-1)
-        mult_stop = mult + 1
-    else:
-        for i in range(mult - 1):
-            mult_string += equation_1[i]
-        multipliers_1.append(int(mult_string))
-        mult_string = ''
-        mult_stop = mult + 1
-    
-    equation_1 = equation_1[mult_stop:]
-    
-    degr_start = equation_1.find('**')
-    
-    if degr_start == -1 and len(equation_1) > 0:
-        degrees_1.append(1)
-        degrees_1.append(0)
-    elif degr_start != -1:
-        if (equation_1.find('+') and equation_1.find('-')) != -1:
-            degr_stop = min(equation_1.find('+'), equation_1.find('-'))
-        elif equation_1.find('+') == -1:
-            degr_stop = equation_1.find('-')
-        else:
-            degr_stop = equation_1.find('+')
-    
-        for i in range(degr_start + 2, degr_stop):
-            degr_string += equation_1[i]
-        degrees_1.append(int(degr_string))
-        degr_string = ''
-        equation_1 = equation_1[degr_stop:]
+del_start = equation_2.find('>')
+equation_2 = equation_2[(del_start + 2):]
+
+print(equation_1)
+print(equation_2)
+
+Separate(equation_1, multipliers_1, degrees_1)
+Separate(equation_2, multipliers_2, degrees_2)
 
 print(multipliers_1)
 print(degrees_1)
+
+print(multipliers_2)
+print(degrees_2)
+
+keys_degrees = []
+keys_degrees.extend(degrees_1)
+
+for i in range(len(degrees_2)):
+    solo = True
+    for j in range(len(keys_degrees)):
+        if degrees_2[i] == keys_degrees[j]:
+            solo = False
+    if solo:
+        keys_degrees.append(degrees_2[i])
+
+keys_degrees.sort(reverse=True)
+        
+dic_equation_1 = {}
+for i in range(len(multipliers_1)):
+    dic_equation_1[degrees_1[i]] = multipliers_1[i]
+
+dic_equation_2 = {}
+for i in range(len(multipliers_2)):
+    dic_equation_2[degrees_2[i]] = multipliers_2[i]
+    
+equation_3 = []
+for i in range(len(keys_degrees)):
+    equation_3.append(dic_equation_1.get(keys_degrees[i], 0) + dic_equation_2.get(keys_degrees[i], 0))
+
+print(equation_3)
+
+k = len(equation_3) - 1
+
+pathWrite = 'Task_5.txt'
+
+with open(pathWrite, 'w', encoding='UTF-8') as data:
+    data.write(f'k = {k} -> ')
+
+if equation_3[0] == 1:
+    with open(pathWrite, 'a', encoding='UTF-8') as data:
+        data.write(f'X**{k}')  
+elif equation_3[0] == -1:
+    with open(pathWrite, 'a', encoding='UTF-8') as data:
+        data.write(f'-X**{k}')
+else:
+    with open(pathWrite, 'a', encoding='UTF-8') as data:
+        data.write(f'{equation_3[0]}*X**{k}')   
+
+for i in range(1, len(equation_3)):
+    if i - k == 0:
+        if equation_3[i] > 0:
+            with open(pathWrite, 'a', encoding='UTF-8') as data:
+                data.write(f'+{equation_3[i]}')
+        elif equation_3[i] < 0:
+            with open(pathWrite, 'a', encoding='UTF-8') as data:
+                data.write(f'{equation_3[i]}')
+    elif k - i == 1:
+        if equation_3[i] > 0:
+            if equation_3[i] == 1:
+                with open(pathWrite, 'a', encoding='UTF-8') as data:
+                    data.write(f'+X')
+            else:
+                with open(pathWrite, 'a', encoding='UTF-8') as data:
+                    data.write(f'+{equation_3[i]}*X')
+        elif equation_3[i] < 0:
+            if equation_3[i] == -1:
+                with open(pathWrite, 'a', encoding='UTF-8') as data:
+                    data.write(f'-X')
+            else:
+                with open(pathWrite, 'a', encoding='UTF-8') as data:
+                    data.write(f'{equation_3[i]}*X')
+    else:
+        if equation_3[i] > 0:
+            if equation_3[i] == 1:
+                with open(pathWrite, 'a', encoding='UTF-8') as data:
+                    data.write(f'+X**{k - i}')
+            else:
+                with open(pathWrite, 'a', encoding='UTF-8') as data:
+                    data.write(f'+{equation_3[i]}*X**{k - i}')
+        elif equation_3[i] < 0:
+            if equation_3[i] == -1:
+                with open(pathWrite, 'a', encoding='UTF-8') as data:
+                    data.write(f'-X**{k - i}') 
+            else:
+                with open(pathWrite, 'a', encoding='UTF-8') as data:
+                    data.write(f'{equation_3[i]}*X**{k - i}')    
+        else:
+            i +=1
