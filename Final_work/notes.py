e20eb58206dad506bd0fa6ff1c4b7c7f5e1e775c
Vedeneev Ilya <tegrozayadc@yandex.ru>
2023-04-24 20:16:03 +0500

    add

--- Final_work/notes.py
+++ Final_work/notes.py
@@ -14,3 +14,19 @@ def read_notes():
 def write_notes(notes):
     with open('notes.json', 'w') as f:
         json.dump(notes, f)
+
+# Функция для добавления новой заметки
+def add_note():
+    notes = read_notes()
+    if not notes:
+        last_id = 0
+    else:
+        last_id = notes[-1]['id']
+    new_id = last_id + 1
+    title = input('Введите заголовок заметки: ')
+    body = input('Введите текст заметки: ')
+    date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    note = {'id': new_id, 'title': title, 'body': body, 'date': date}
+    notes.append(note)
+    write_notes(notes)
+    print(f'Заметка с id {new_id} добавлена')
