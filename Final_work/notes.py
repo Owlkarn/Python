873521785a85f8ee074833a7d56b4c9d68a84714
Vedeneev Ilya <tegrozayadc@yandex.ru>
2023-04-24 19:37:37 +0500

    read and write

--- Final_work/notes.py
+++ Final_work/notes.py
@@ -1,1 +1,16 @@
+import json
+import datetime
 
+# Функция для чтения заметок из файла
+def read_notes():
+    try:
+        with open('notes.json', 'r') as f:
+            notes = json.load(f)
+    except FileNotFoundError:
+        notes = []
+    return notes
+
+# Функция для записи заметок в файл
+def write_notes(notes):
+    with open('notes.json', 'w') as f:
+        json.dump(notes, f)
